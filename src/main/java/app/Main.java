666e9bb5b955507e6ff79300040b3ecf3841d7b9
Vincent <vincentwangvw05@gmail.com>
2024-11-04 15:36:24 -0500

    setup

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Main {
+    /**
+     * The main method for starting the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
