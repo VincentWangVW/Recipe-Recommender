f28b78d41fb988db698dd8781deaac782ce1d7d1
Zife1Luo <228phyllis@gmail.com>
2024-12-03 18:53:29 -0500

    fixed checkstyle error for main in interface_adapter

--- src/main/java/interface_adapter/main/MainController.java
+++ src/main/java/interface_adapter/main/MainController.java
@@ -2,21 +2,47 @@ package interface_adapter.main;
 
 import use_case.mainwindow.MainInputBoundary;
 
+/**
+ * Controller for handling main screen actions and navigation.
+ * Responsible for delegating user interactions to the MainInputBoundary interactor.
+ */
 public class MainController {
     private final MainInputBoundary mainInteractor;
 
+    /**
+     * Constructs a MainController with the given MainInputBoundary.
+     *
+     * @param mainInteractor The interactor for handling main screen actions.
+     */
     public MainController(MainInputBoundary mainInteractor) {
         this.mainInteractor = mainInteractor;
     }
-    public void switch_to_DateView() {
+
+    /**
+     * Switches the view to the Date screen.
+     */
+    public void switchToDateView() {
         mainInteractor.switchToDateView();
     }
 
-    public void switch_to_RecipeView() {
+    /**
+     * Switches the view to the Recipe screen.
+     */
+    public void switchToRecipeView() {
         mainInteractor.switchToRecipeView();
     }
 
-    public void switch_to_IngredientsView(){mainInteractor.switchToIngredientsView(); }
+    /**
+     * Switches the view to the Ingredients screen.
+     */
+    public void switchToIngredientsView() {
+        mainInteractor.switchToIngredientsView();
+    }
 
-    public void switch_to_UserInfoView(){mainInteractor.switchToUserInfoView(); }
+    /**
+     * Switches the view to the User Information screen.
+     */
+    public void switchToUserInfoView() {
+        mainInteractor.switchToUserInfoView();
+    }
 }
--- src/main/java/interface_adapter/main/MainPresenter.java
+++ src/main/java/interface_adapter/main/MainPresenter.java
@@ -1,12 +1,16 @@
 package interface_adapter.main;
 
 import interface_adapter.ViewManagerModel;
+import interface_adapter.datescreen.DateViewModel;
 import interface_adapter.ingredients_manager.IngredientsViewModel;
 import interface_adapter.recommend_recipes.RecipesViewModel;
-import interface_adapter.datescreen.DateViewModel;
 import interface_adapter.user_info.UserInfoViewModel;
 import use_case.mainwindow.MainOutputBoundary;
 
+/**
+ * Presenter for handling transitions between different views in the main interface.
+ * Responsible for updating the {@link ViewManagerModel} with the appropriate state based on user actions.
+ */
 public class MainPresenter implements MainOutputBoundary {
     private final ViewManagerModel viewManagerModel;
     private final DateViewModel dateViewModel;
