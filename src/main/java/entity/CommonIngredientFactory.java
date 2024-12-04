509f5bfd58664256236df54f262da629e0c2a664
richardwang1236 <wangyc2005@qq.com>
2024-12-03 21:40:38 -0500

    javadoc

--- src/main/java/entity/CommonIngredientFactory.java
+++ src/main/java/entity/CommonIngredientFactory.java
@@ -1,6 +1,16 @@
 package entity;
 
+/**
+ * A factory for creating common ingredients.
+ */
 public class CommonIngredientFactory implements IngredientFactory {
+
+    /**
+     * Creates a common ingredient with the given name and amount.
+     * @param name the name of the ingredient
+     * @param amount the amount of the ingredient
+     * @return a common ingredient with the given name and amount
+     */
     @Override
     public Ingredient create(String name, int amount) {
         return new CommonIngredient(name, amount);
