7fa221df9868f36fdbfae396566eff8558a540e6
richardwang1236 <wangyc2005@qq.com>
2024-12-03 21:55:05 -0500

    javadoc

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,6 +4,32 @@ import javax.swing.JFrame;
 
 /**
  * Main class to run the application.
+ * <p>
+ * # Recipe Recommender - CSC207 Group 166
+ * <p>
+ * This application helps users generate recipes based on the ingredients they have, dietary restrictions, and other preferences.
+ * Users can also generate recipes based on holidays, seasons, or custom searches.
+ * The app provides a user-friendly interface to manage ingredients, user information, and generate recipes.
+ * <p>
+ * The application includes several views:
+ * - Main View: The main interface of the application.
+ * - Date View: Displays the current date, holiday, and season information.
+ * - Recipe View: Allows users to generate recipes based on various criteria.
+ * - Ingredients View: Manages the ingredients available to the user.
+ * - Generated Recipes View: Displays the generated recipes with details.
+ * - User Info View: Manages user-specific information such as dietary restrictions and allergies.
+ * <p>
+ * The application was developed to help users make the most out of their pantry items and improve their cooking skills.
+ * It aims to reduce food waste by suggesting recipes based on available ingredients and user preferences.
+ * The app also supports custom searches, making it versatile for various cooking needs.
+ * <p>
+ * Authors:
+ * - Vincent Wang
+ * - Laith Al Khoury
+ * - Xinni Liu
+ * - Yuchen Wang
+ * - Jiahe Xiang
+ * - Zifei Luo
  */
 public class Main {
     /**
